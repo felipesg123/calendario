5e42d2cd661d489122056247463968626ae4e5fb
felipesg123 <goncalves.silva@alunoescolassociais.org.br>
2024-04-05 07:07:05 -0300

    add arquivos modificados

--- calendar.js
+++ calendar.js
@@ -1,1 +1,7 @@
-
+function colorirDia(){
+    let days        = document.getElementById("day").value;
+    let color       = document.getElementById('color').value;
+    let calendar    = document.getElementById('calendar');
+    let td          = calendar.getElementsByTagName('td')[(parseInt(days)+2)];
+    td.style.backgroundColor = color;
+}
